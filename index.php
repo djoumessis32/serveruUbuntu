4c1e66d66ef6a1e215f04ba2f1997cff53ae2d73
djoumessis32 <51720551+djoumessis32@users.noreply.github.com>
2022-02-22 01:41:08 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,25 @@
-<?php
-  <?php
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>  
+        <?php
             $servername = 'localhost';
             $username = 'root';
-            $password = '';
+            $password = 'root';
             
             //On établit la connexion
-            $conn = new mysqli($servername, $username, $password);
+            $conn = mysqli_connect($servername, $username, $password);
             
             //On vérifie la connexion
-            if($conn->connect_error){
-                die('Erreur : ' .$conn->connect_error);
+            if(!$conn){
+                die('Erreur : ' .mysqli_connect_error());
             }
             echo 'Connexion réussie';
         ?>
-?>
+    </body>
+</html>
